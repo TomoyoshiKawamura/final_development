09c651b27644a5c47fb62ea671501a30bee98c11
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-10-25 16:36:06 +0900

    Commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  # before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   # GET /items
   # GET /items.json
@@ -63,9 +63,9 @@ class ItemsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_item
-      @item = Item.find(params[:id])
-    end
+    # def set_item
+    #   @item = Item.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
