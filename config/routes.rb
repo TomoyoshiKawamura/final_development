8256ade80f91cee8ad609f23195f3fe24d74a615
mi-yasuoka8102 <mizukiyasuoka23@gmail.com>
2018-10-26 16:11:59 +0900

    Add devise, edit header
    deviseの導入
    ヘッダーにログイン関連のリンクの実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
   resources :items
   resources :cart_items
   resources :carts
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
