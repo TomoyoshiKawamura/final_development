f6ebf246880f05878f8ffaebc9eb83fd0a31f5cf
mi-yasuoka8102 <mizukiyasuoka23@gmail.com>
2018-10-31 11:45:13 +0900

    Edit gitignore
    画像がにアップロードしないように編集。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -67,12 +67,10 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
       params.require(:item).permit(:name, :price, :company, :explanation)
     end
