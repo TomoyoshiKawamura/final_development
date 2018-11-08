bcbd1815dbc7f521506063afac707187973c0d26
TomoyoshiKawamura <tomo.inf.universe@gmail.com>
2018-11-08 15:52:59 +0900

    トップの中央揃え

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,5 @@
 class Cart < ApplicationRecord
     belongs_to :user
-    has_many :item, through: :cart_items
+    has_many :cart_items #丸々抜けてた
+    has_many :items, through: :cart_items #sが抜けてた
 end
