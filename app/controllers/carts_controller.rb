a1964a69c4414c02e749099a98fc1275262a2689
野本佳裕 <jiayuyeben@gmail.com>
2018-11-08 18:20:14 +0900

    preparing-for-main-function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  # before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
   # GET /carts
   # GET /carts.json
   def index
@@ -10,6 +10,7 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    @cart_items = current_cart.cart_items
   end
 
   # GET /carts/new
@@ -37,8 +38,29 @@ class CartsController < ApplicationController
     end
   end
 
+  def add_item
+    # if @cart_item.blank?
+    #   @cart_item = current_cart.cart_items.build(product_id: params[:product_id])
+    # end
+  
+    # @cart_item.quantity += params[:quantity].to_i
+    # @cart_item.save
+    # redirect_to current_cart
+  end
+
+  # def update_item
+  #   @cart_item.update(quantity: params[:quantity].to_i)
+  #   redirect_to current_cart
+  # end
+
+  # def delete_item
+  #   @cart_item.destroy
+  #   redirect_to current_cart
+  # end
+
   # PATCH/PUT /carts/1
   # PATCH/PUT /carts/1.json
+  
   def update
     respond_to do |format|
       if @cart.update(cart_params)
@@ -53,6 +75,7 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1
   # DELETE /carts/1.json
+  
   def destroy
     @cart.destroy
     respond_to do |format|
@@ -61,14 +84,20 @@ class CartsController < ApplicationController
     end
   end
 
-  private
+  # private
     # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
+    
+    # def set_cart
+    #   @cart = Cart.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_params
-      params.require(:cart).permit(:user_id)
-    end
+    # def cart_params
+    #   params.require(:cart).permit(:user_id)
+    # end
+    
+    # def setup_cart_item!
+    #   @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+    # end
+
 end
