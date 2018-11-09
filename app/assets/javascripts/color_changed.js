da20df02df7fe893f1c6cbd27211f92d98965e25
mi-yasuoka8102 <mizukiyasuoka23@gmail.com>
2018-11-09 17:10:26 +0900

    Edit color_changed.js
    購入完了画面の他のユーザーが買った商品の名前も変化するようにする。

--- app/assets/javascripts/color_changed.js
+++ app/assets/javascripts/color_changed.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('.search-item-name, .inside-of-shopping-cart-item__explanation-title, .recommend-item-name').hover(function() {
+    $('.search-item-name, .inside-of-shopping-cart-item__explanation-title, .recommend-item-name, .another-user-item-name').hover(function() {
         $(this).css('color', '#c4562b'); //文字の色を変更する　色は元サイトに準じている
     }, function() {
         $(this).css('color', '');
